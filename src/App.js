27fc0a6dd432b88fe63b013e30ac8484e4a05bc9
Julius Markauskas <juliusm9791@gmail.com>
2022-01-23 22:18:28 -0500

    navLinks update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import Portfoliomain from './components/portfoliomain';
 
 function App() {
   return (
-    <div>
-      <Portfoliomain/>
-    </div>
+    <>
+      <Portfoliomain />
+      <footer className="fixed-bottom mt-auto pt-2 bg-light">
+        <div className="container">
+          <p className="text-muted">© Julius Markauskas, 2022</p>
+        </div>
+      </footer>
+    </>
   );
 }
 
