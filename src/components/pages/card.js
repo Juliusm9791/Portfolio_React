fe2a1b98ab10c775778ba9054541aaa8f2ca8e7a
Julius Markauskas <juliusm9791@gmail.com>
2022-03-04 21:26:33 -0500

    working on flipping card

--- src/components/pages/card.js
+++ src/components/pages/card.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBRow, MDBCol, MDBIcon, MDBView, MDBMask } from 'mdbreact';
 import fixTitleName from '../../utils/correctData';
 import returnLink from '../../utils/images';
+import ReactCardFlipper from "react-card-flipper";
 
 
 export default function Card() {
@@ -33,39 +34,46 @@ export default function Card() {
     // }, []);
 
     const combineRepo = [...repos] || {};
-    
 
+console.log(combineRepo)
     const renderedCards = combineRepo.map((repo) => {
         if (repo.name !== "Portfolio" && repo.name !== "prework-about-me" && repo.name !== "Readme_Generator" && repo.name !== "Team_Profile_Generator" && repo.name !== "Team_Profile_Generator_HTML" && repo.name !== "Note_Taker" && repo.name !== "Employee_Tracker" && repo.name !== "E-Commerce" && repo.name !== "Social_Network_API" && repo.name !== "Text_Editor" && repo.name !== "Portfolio_React" && repo.name !== "Redux_Store" && repo.name !== "Book_Search_Engine") {
             return (
-                <MDBCol md='4' className='p-4' key={repo.id} >
-                    <MDBCard>
-                        <a href={repo.homepage} target='_blank'>
-                            <MDBCardImage
-                                top
-                                src={returnLink(repo.name)}
-                                overlay='black-light'
-                                hover
-                                waves
-                                alt={returnLink(repo.name)}
-                            /></a>
-                        <MDBCardBody className='elegant-color white-text rounded-bottom'>
-                            <hr className='hr-light' />
-                            <MDBCardTitle>{fixTitleName(repo.name)}</MDBCardTitle>
-                            <hr className='hr-light' />
-                            <MDBCardText className='white-text'>
-                                {repo.description}
-                            </MDBCardText>
-                            <a href={repo.svn_url} className='black-text d-flex justify-content-end'>
-                                <h5 className='white-text'>
-                                    <MDBIcon fab icon='github' />    Read more
-                                    <MDBIcon icon='angle-double-right' className='ml-2' />
-                                </h5>
-                            </a>
-                        </MDBCardBody>
-                    </MDBCard>
-                </MDBCol>
-
+                <ReactCardFlipper width="300px" height="550px" behavior="click" levitate={true}>
+                    <div>
+                        <MDBCol  className='p-4' key={repo.id} >
+                            <MDBCard>
+                                <a href={repo.homepage} target='_blank'>
+                                    <MDBCardImage
+                                        top
+                                        src={returnLink(repo.name)}
+                                        overlay='black-light'
+                                        hover
+                                        waves
+                                        alt={returnLink(repo.name)}
+                                    /></a>
+                                <MDBCardBody className='elegant-color white-text rounded-bottom'>
+                                    <hr className='hr-light' />
+                                    <MDBCardTitle>{fixTitleName(repo.name)}</MDBCardTitle>
+                                    <hr className='hr-light' />
+                                    <MDBCardText className='white-text'>
+                                        {repo.description}
+                                    </MDBCardText>
+                                    <a href={repo.svn_url} className='black-text d-flex justify-content-end'>
+                                        <h5 className='white-text'>
+                                            <MDBIcon fab icon='github' />    Read more
+                                            <MDBIcon icon='angle-double-right' className='ml-2' />
+                                        </h5>
+                                    </a>
+                                </MDBCardBody>
+                            </MDBCard>
+                        </MDBCol>
+                    </div>
+                    <div>
+                       <h>Tecnnology used:</h> 
+                       {repo.topics.map((texh) => { return <p>{texh}</p>})}
+                       </div>
+                </ReactCardFlipper>
             );
         }
     });
